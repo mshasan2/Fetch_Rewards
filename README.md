8c039858fcab3979465ac0d55a7ac34379300657
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2024-04-19 07:17:18 -0400

    Adding Requirements and updating Readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-Fetch Coding Exercise
+# Fetch Coding Exercise
+
+**Problem:** Given a balance scale and 9 gold bars of the same size and look. You don’t know the exact weight of each bar, but you know they all weigh the same, except for one fake bar. It weighs less than others. You need to find the fake gold bar by only bars and balance scales. You can only place gold bars on scale plates (bowls) and find which scale weighs more or less.
+
+**Solutions**
+
+**Note:** As time is a constraint, we will hardfit the solution for 9 bars. The solution can be generalized for n bars using the same approach.
+
+1. **Brute Force:** Compare each bar with each other till we find the lighter bar.
+2. **Divide and Conquer:** 
+    1. Divide the bars into 3 groups of 3 bars each. 
+    2. Weigh two groups.
+    3. If they are equal, the fake bar is in the third group.
+    4. If they are not equal, the fake bar is in the lighter group.
+    5. Repeat the process with the lighter group. 
+    6. Continue until you find the fake bar.
+
+We will implement the second solution as it is more efficient.
+
+**Implementation**: 
+1. We will implement the solution in Python.
+2. Selenium WebDriver will be used to automate the browser.
+**Note:** We have added a delay of 2 seconds in multiple places to make the process visible. This can be removed to make the process faster when implemented as part of an automated system.
+
+**Steps to run the code:**
+1. Install Python 3.8 or higher.
+2. Clone this repository using the command `git clone https://github.com/mshasan2/Fetch_Rewards.git`.
+3. Install the required libraries using the command `pip install -r requirements.txt`.
+4. Navigate to the directory where the code is cloned.
+5. Run the command `python findFakeBar.py` in terminal / command prompt. The result will be displayed in the terminal.
+
--- findFakeBar.py
+++ findFakeBar.py
@@ -6,13 +6,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+# Create a new instance of the Chrome driver
 service = Service(exceutable_path='chromedriver')
 driver = webdriver.Chrome(service=service)
 
+# Navigate to the application home page
 driver.get('http://sdetchallenge.fetch.com/')
 
+# Wait for the page to load
 WebDriverWait(driver, 15).until(
     EC.presence_of_element_located((By.ID, 'root')))
+
+
+def printWeighing():
+    # This function will print the number of weighings and the list of weighings
+    items = driver.find_elements(By.XPATH, "/html/body/div/div/div[1]/div[5]/ol/li")
+    print("Number of weighings: ", len(items))
+    print("List of weighings: ")
+    for item in items:
+        print(item.text)
+    return
 
 def clickResult(driver, resultCoin):
     # This function will click the result button and catch the alert
@@ -21,18 +34,20 @@ def clickResult(driver, resultCoin):
     # Catch the alert
     WebDriverWait(driver, 15).until(
         EC.alert_is_present())
-    time.sleep(2)
     completion_alert = driver.switch_to.alert
     text = completion_alert.text
-    if text == 'Yay! You find it!':
-        print("Fake bar found successfully")
+    print("Alert Message: ", text)
+    # Adding a delay of 2 seconds to manually check the result
+    time.sleep(2)
     completion_alert.accept()
-    
+    printWeighing()
+    return
 
 def clickReset(driver):
     # This function will click the reset button
     reset = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[4]/button[1]")
     reset.click()
+    return
     
 
 def findBar(left, right, remaining):
@@ -46,7 +61,8 @@ def findBar(left, right, remaining):
     weigh = driver.find_element(By.ID, 'weigh')
     weigh.click()
 
-    time.sleep(3)
+    # Adding a delay of 2 seconds to manually check the result
+    time.sleep(2)
 
     result_element = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[2]/button")
     if result_element.text == '=':
@@ -54,7 +70,6 @@ def findBar(left, right, remaining):
             clickResult(driver, "coin_"+remaining[0])
         else:
             clickReset(driver)
-            print("rem", remaining)
             findBar(remaining[0], remaining[1], remaining[2])
     elif result_element.text == '<':
         if len(left) == 1:
@@ -71,9 +86,12 @@ def findBar(left, right, remaining):
 
     return
 
-
+# Start the process
 findBar(['0','1','2'],['3','4','5'],['6','7','8'])
 
+# Wait for 2 seconds before closing the browser, so that we can see the final result
+# The below line can be removed if we don't want to see the final result
 time.sleep(2)
 
+# Close the browser
 driver.quit()
